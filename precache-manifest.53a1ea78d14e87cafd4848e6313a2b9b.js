61bbc5ddee599bf456f20d6a8cdd3cb9761fe3a0
Liudmila Mzhachikh <l.mzhachikh@corp.mail.ru>
2019-04-29 15:06:21 +0300

    Updates

---
+++ precache-manifest.53a1ea78d14e87cafd4848e6313a2b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400556e6a79411644f755e94f5a6fe1a",
+    "url": "/cherries-test/index.html"
+  },
+  {
+    "revision": "192cb3b8e90cc5c7d445",
+    "url": "/cherries-test/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cherries-test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "192cb3b8e90cc5c7d445",
+    "url": "/cherries-test/static/js/main.ce760c1b.chunk.js"
+  },
+  {
+    "revision": "373cd46ee8a16aae8e4a",
+    "url": "/cherries-test/static/js/runtime~main.ad32530a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cherries-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
